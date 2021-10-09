94d6ce63331359094d9ba289f6061d92b32b9eef
YuYoung <2901161079@qq.com>
2021-10-09 09:27:42 +0800

    init commit

---
+++ src/main/java/com/yuyoung/hdugame/HduGameApplication.java
@@ -0,0 +1,13 @@
+package com.yuyoung.hdugame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HduGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HduGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuyoung/hdugame/HduGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuyoung.hdugame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HduGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
